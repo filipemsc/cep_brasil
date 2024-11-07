1e5ddd9f85760c7702a2cf4e46f524a35816f607
Filipe Cavalcanti <f.matheus.cavalcanti@gmail.com>
2024-11-07 15:02:57 -0300

    initial-commit

---
+++ cep_download.R
@@ -0,0 +1,14 @@
+temp <- tempfile()
+download.file("https://cnes.datasus.gov.br/EstatisticasServlet?path=CEP_BRASIL.ZIP", "CEP_BRASIL.zip", mode='wb')
+
+file_cep <- unz("CEP_BRASIL.zip", "CEP.txt")
+file_cep_alterado <- unz("CEP_BRASIL.zip", "CEPALTERADO.txt")
+
+cep <- read.delim(file_cep, header=FALSE) 
+names(cep) <- c("cep", "sigla_uf", "ibge6")
+
+cep_alterado <- read.delim(file_cep_alterado, header=FALSE)
+names(cep_alterado) <- c("cep", "sigla_uf", "ibge6", "alteracao")
+
+saveRDS(cep, "cep.rds")
+saveRDS(cep_alterado, "cep_alterado.rds")
